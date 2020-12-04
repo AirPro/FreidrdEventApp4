9a9b249d42718459394939ea18762451aa7fcdd6
Bob Freid <freidrd@mail.uc.edu>
2020-12-04 09:48:42 -0500

    created repository abd irepository

---
+++ Models/DataLayer/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using FreidrdEventApp4.Models.DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreidrdEventApp4.Models.DataLayer
+{
+	public interface IRepository<T> where T : Event
+	{
+		IEnumerable<T> List(QueryOptions<T> options);
+		T get(int id);
+		void Insert(T entity);
+		void Update(T entity);
+		void Delete(T entity);
+		void Save();
+	}
+}
