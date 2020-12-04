1fee05997f31eb49492e90f38fa6e84ed9349ea4
Bob Freid <freidrd@mail.uc.edu>
2020-12-04 09:30:31 -0500

    Create DomainModel Event.cs class

---
+++ Models/DomainModel/Event.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreidrdEventApp4.Models.DomainModel
+{
+	public class Event
+	{
+        public int EventId { get; set; }
+
+        [StringLength(50)]
+        [Required]
+        public string EventName { get; set; }
+
+        [StringLength(50)]
+        [Required]
+        public string EventCity { get; set; }
+
+        [StringLength(50)]
+        [Required]
+        public string EventDate { get; set; }
+
+        [StringLength(50)]
+        [Required]
+        public string VenueName { get; set; }
+
+        [StringLength(50)]
+        [Required]
+        public string VenueAddress { get; set; }
+
+        [StringLength(50)]
+        [Required]
+        public string PromoterName { get; set; }
+    }
+}
