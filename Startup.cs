5683ad82d39ed050a8b116bf1a4330300301299d
Bob Freid <freidrd@mail.uc.edu>
2020-12-04 09:56:56 -0500

    added service to startup file

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,8 @@
+using FreidrdEventApp4.Models.DataLayer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,7 @@ namespace FreidrdEventApp4
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddControllersWithViews();
+			services.AddDbContext<FreidrdFinalProjectContext>(options => options.UseSqlServer(Configuration.GetConnectionString("FreidrdFinalProjectContext")));
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
