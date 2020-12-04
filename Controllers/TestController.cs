04e52c6ba7b3d2b1be92a9000150fb90c763748d
Bob Freid <freidrd@mail.uc.edu>
2020-12-04 10:36:33 -0500

    Added views and 2 controllers

---
+++ Controllers/TestController.cs
@@ -0,0 +1,56 @@
+﻿using FreidrdEventApp4.Models.DataLayer;
+using FreidrdEventApp4.Models.DomainModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreidrdEventApp4.Controllers
+{
+	public class TestController : Controller
+	{
+		private Repository<Event> events { get; set; }
+
+		public TestController(FreidrdFinalProjectContext ctx) => events = new Repository<Event>(ctx);
+
+		public ViewResult Index()
+		{
+			var options = new QueryOptions<Event>
+			{
+				OrderBy = e => e.EventId
+			};
+			return View(events.List(options));
+		}
+		[HttpGet]
+		public ViewResult Add() => View();
+
+		[HttpPost]
+		public IActionResult Add(Event e)
+		{
+			if (ModelState.IsValid)
+			{
+				if (e.EventId == 0)
+					events.Insert(e);
+				else
+					events.Update(e);
+				events.Save();
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				return View(events);
+			}
+		}
+
+		[HttpGet]
+		public ViewResult delete(int id)
+		{
+			return View(events.get(id));
+		}
+
+		[HttpPost]
+		public RedirectToActionResult Delete(Event e)
+		{
+			events.Delete(e);
+			events.Save();
+			return RedirectToAction("index");
+		}
+	}
+}
---
+++ Migrations/20201204153051_Initial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FreidrdEventApp4.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    EventId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EventName = table.Column<string>(maxLength: 50, nullable: false),
+                    EventCity = table.Column<string>(maxLength: 50, nullable: false),
+                    EventDate = table.Column<string>(maxLength: 50, nullable: false),
+                    VenueName = table.Column<string>(maxLength: 50, nullable: false),
+                    VenueAddress = table.Column<string>(maxLength: 50, nullable: false),
+                    PromoterName = table.Column<string>(maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.EventId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Events");
+        }
+    }
+}
