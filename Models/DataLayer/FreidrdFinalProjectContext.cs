4bd83536606f14a9b06485f2982ca350deb052c6
Bob Freid <freidrd@mail.uc.edu>
2020-12-04 09:37:33 -0500

    Added the context file

---
+++ Models/DataLayer/FreidrdFinalProjectContext.cs
@@ -0,0 +1,19 @@
+﻿using FreidrdEventApp4.Models.DomainModel;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreidrdEventApp4.Models.DataLayer
+{
+	public class FreidrdFinalProjectContext : DbContext
+	{
+		public FreidrdFinalProjectContext(DbContextOptions<FreidrdFinalProjectContext> options)
+			: base(options)
+		{
+		}
+
+		public virtual DbSet<Event> Events { get; set; }
+	}
+}
