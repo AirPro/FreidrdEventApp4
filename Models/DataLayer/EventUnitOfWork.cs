352e8bd2fe297f6dbbc96ec524122ca694b91868
Bob Freid <freidrd@mail.uc.edu>
2020-12-04 09:53:04 -0500

    Unit Of Work Files created and saved

---
+++ Models/DataLayer/EventUnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using FreidrdEventApp4.Models.DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreidrdEventApp4.Models.DataLayer
+{
+    public class EventUnitOfWork : IEventUnitOfWork
+    {
+        private FreidrdFinalProjectContext context { get; set; }
+        public EventUnitOfWork(FreidrdFinalProjectContext ctx) => context = ctx;
+
+        private Repository<Event> eventData;
+        public Repository<Event> Events
+        {
+            get
+            {
+                if (eventData == null)
+                    eventData = new Repository<Event>(context);
+                return eventData;
+            }
+        }
+
+        public void Save() => context.SaveChanges();
+    }
+}
---
+++ Models/DataLayer/IEventUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using FreidrdEventApp4.Models.DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreidrdEventApp4.Models.DataLayer
+{
+	public interface IEventUnitOfWork
+	{
+		public Repository<Event> Events { get; }
+
+		public void Save();
+	}
+}
